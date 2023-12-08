9055f035d087af5d7ae6d8ccd3e95a8ea7cf627f
Vladimir <w112vovan@mail.ru>
2023-12-08 16:16:27 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L43_shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class Player    // Дописать
+    {
+        private int _money;
+        private int _massLimit;
+        private List<Product> _bag = new List<Product>();
+
+        public Player(int money, int massLimit) // Дописать
+        {
+            _money = money;
+            _massLimit = massLimit;
+        }
+
+        public void BuyProduct()            // Дописать
+        {
+
+        }
+
+        public void ShowInventory()     // Дописать
+        {
+
+        }
+    }
+
+    class Vendor    // Дописать
+    {
+        private int _money;
+        private List<Product> _storage = new List<Product>();
+
+        public Vendor()
+        {
+            _money = 0;
+        }
+
+        public void AddProduct()    // Дописать
+        {
+
+        }
+        public void SellProduct()  // Дописать. Поиск ближайшего продукта по имени
+        {
+
+        }
+
+        public void ShowProducts()   // Дописать
+        {
+
+        }
+    }
+
+    class Product
+    {
+        private string _name;
+        private int _mass;
+
+        public Product(string name, int mass)
+        {
+            _name = name;
+            _mass = mass;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+
+        public int Mass
+        {
+            get
+            {
+                return _mass;
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("L43_shop")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("L43_shop")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("15109267-d573-4f1b-a2c0-8ddd737caaa4")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
