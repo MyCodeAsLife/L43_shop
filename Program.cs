ec17e6d61c965a7fccb17e3b07be4e9866628dc2
Vladimir <w112vovan@mail.ru>
2023-12-11 18:58:23 +0500

    Правка 1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace L43_shop
 {
